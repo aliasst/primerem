27a2dcc8dd366ce14d59c20095a9f5820b22ed44
aliasst <ahtubafishing@yandex.ru>
2026-04-22 18:51:31 +0300

    test

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/test', [\App\Http\Controllers\Cabinet\ProjectStageController::class, 'test']);
 
-
+//Route::get('/', HomeController::class)->name('home');
 
 Route::get('/', HomeController::class)->name('home');
 
