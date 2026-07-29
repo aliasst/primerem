3edcff1a8b5e04767b4384ed94816935cddd889c
aliasst <ahtubafishing@yandex.ru>
2026-07-29 17:48:43 +0300

    fdd

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
 <head>
     <meta charset="UTF-8" />
+    <meta name="language" content="Russian">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <!--<link rel="shortcut icon" href="favicon.ico?v=1.2" type="image/x-icon" />-->
     <link rel="stylesheet" href="/css/reset.css" />
